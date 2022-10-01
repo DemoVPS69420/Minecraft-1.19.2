1c1cffed50fd4a7b941b01925ab44b7b7b2701a1
DemoVPS69420 <111243610+DemoVPS69420@users.noreply.github.com>
2022-09-30 23:40:03 -0700

    Minecraft Version 1.19.2 Patch 1

---
+++ CommandLineMinecraft.sh
@@ -0,0 +1,1 @@
+tailscale up && apt install default-jre && apt install openjdk-16-jre-headless openjdk-17-jre-headless && wget https://piston-data.mojang.com/v1/objects/f69c284232d7c7580bd89a5a4931c3581eae1378/server.jar
